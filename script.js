4a373d8c6b4a16192c4c1e2f7b0563251d5c9216
JYOTIKA07 <kadwajyotika@gmail.com>
2024-10-23 20:36:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.querySelector('.value');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( (item)  => {
+    item.onclick = ()  
+
+        
+
+
+        
+
+    }
+})
